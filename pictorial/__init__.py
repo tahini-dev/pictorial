0980233e3107d1c7c2f275c93de1d9285861bc10
tahini-dev <tahini.dev@gmail.com>
2021-11-14 16:20:53 +0000

    better init

--- pictorial/__init__.py
+++ pictorial/__init__.py
@@ -1,10 +1,7 @@
-import pictorial.bar
-import pictorial.box
+from pictorial.bar import plot as bar
+from pictorial.box import plot as box
 
 __all__ = [
     'bar',
     'box',
 ]
-
-bar = pictorial.bar.plot
-box = pictorial.box.plot
